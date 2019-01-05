49b0b2eda4402c0fedf98de457328c8cd55d9266
Vaibhav Kumar Tiwari <42978623+BanarasiVaibhav@users.noreply.github.com>
2019-01-06 01:37:10 +0530

    Create ExploreInbuiltFunctions.py

---
+++ ExploreInbuiltFunctions.py
@@ -0,0 +1,9 @@
+# learning is an art 
+# this program is to explain how we can explore inbuilt functions
+
+print(dir(123))
+# this line will list all the operation that we can do with builtin functions on integer numbers
+
+
+print(dir("Hello"))
+# this line will list all the operation that we can do with builtin functions on strings
