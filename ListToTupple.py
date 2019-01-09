c009ac3bee52215ca0e2c6d12ae066ca9770ff73
Vaibhav Kumar Tiwari <42978623+BanarasiVaibhav@users.noreply.github.com>
2019-01-09 23:55:47 +0530

    Create ListToTupple.py

---
+++ ListToTupple.py
@@ -0,0 +1,10 @@
+# Converting list to toople
+
+lst=[1,2,3,4,5,6,7,8]
+topple=tuple(lst)
+print(topple)
+
+#converting list to tupple
+tpple=('a','s','d',1,2,3,4)
+Newlist=list(tpple)
+print(Newlist)
