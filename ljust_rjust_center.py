232a40d18e637b804ac3a6c3d733ba545f242ccd
Vaibhav Kumar Tiwari <42978623+BanarasiVaibhav@users.noreply.github.com>
2019-02-26 04:35:05 +0530

    Create ljust_rjust_center.py

---
+++ ljust_rjust_center.py
@@ -0,0 +1,7 @@
+name=' kya karoge jaan kar '
+sorry='I am sorry my name is Vaibhav'
+print(' its alright '.join([name,sorry]))
+print(name.split())
+print(name.rjust(100,'-'))
+print(name.ljust(100,'0'))
+print(name.center(100,'*'))
