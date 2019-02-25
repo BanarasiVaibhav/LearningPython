88554edfaa1d2fa01ec188567da6a52c47884a09
Vaibhav Kumar Tiwari <42978623+BanarasiVaibhav@users.noreply.github.com>
2019-02-26 04:49:15 +0530

    Update ljust_rjust_center.py

--- ljust_rjust_center.py
+++ ljust_rjust_center.py
@@ -5,3 +5,12 @@ print(name.split())
 print(name.rjust(100,'-'))
 print(name.ljust(100,'0'))
 print(name.center(100,'*'))
+
+
+
+def printPicnic(itemsDict,leftWidth,rightWidth):
+    print('PICNIC ITEMS'.center(leftWidth+rightWidth,'-'))
+    for k,v in itemsDict.items():
+        print(k.ljust(leftWidth)+str(v.rjust(rightWidth)))
+picnicItems={'sandwiche':'6','maggi':'10','mat':'full size','camera':'190'}
+printPicnic(picnicItems,18,18)
