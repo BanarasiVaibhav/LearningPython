4ddc4b4001509ee76c9822a57d524de7b05b94ec
Vaibhav Kumar Tiwari <42978623+BanarasiVaibhav@users.noreply.github.com>
2019-04-11 18:46:32 +0530

    Update FileHandling.py

--- FileHandling.py
+++ FileHandling.py
@@ -1,6 +1,8 @@
+# program to append numbers 0 to 99 in a text file
+
 import os
 helloFile=open('C:\\Users\\Vaibhav Tiwari\\PYTHON.txt','a')
 for i in range(0,100):
-    helloFile.write(str(i))
-    helloFile.write('\n')
+    helloFile.write(str(i))  #write numbers
+    helloFile.write('\n')    #newline character
 
